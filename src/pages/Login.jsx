464d9df28717a443f7b7f3562516d1aa10adcfdf
Mathis Engels <mathisengels@yahoo.fr>
2021-09-20 22:22:09 +0200

    refractor (prettier)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,21 +24,13 @@ function Login({ setToken }) {
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-            }}>
+            }}
+        >
             <Typography component="h1" variant="h5">
                 Log in
             </Typography>
             <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
+                <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
                 <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
                     Log In
                 </Button>
