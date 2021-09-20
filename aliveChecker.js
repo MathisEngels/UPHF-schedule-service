185df646dfb764e4b915ad166a0cadee15a13e15
Mathis Engels <mathisengels@yahoo.fr>
2021-09-20 14:35:50 +0200

    add a alive checker to have some funni stats

---
+++ aliveChecker.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const path = require("path");
+const { axios } = require("axios");
+
+
+// TODO Switch to TS fast
+class UPHFAliveChecker {
+    constructor(dataFolderPath, interval) {
+        this.dataFolderPath = dataFolderPath;
+        this.dataFilePath = path.join(dataFolderPath, `status.json`);
+        this.interval = Math.max(5, interval);
+        this.intervalID = null;
+        this.data = null;
+    }
+
+    async start() {
+        await this.createFoldersIfNeeded();
+        await this.loadDB();
+
+        await this.check();
+        this.startInterval();
+    }
+
+    async stop() {
+        this.stopInterval();
+    }
+
+    toObject() {
+        return this.data;
+    }
+
+    async check() {
+        const alive = await this.request();
+
+        this.updateDB(alive);
+    }
+
+    async request() {
+        try {
+            await axios.get("https://vtmob.uphf.fr/esup-vtclient-up4/stylesheets/home.xhtml?m=false", { timeout: 1000 });
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    startInterval() {
+        this.intervalID = setInterval(this.check, this.interval * 1000 * 60);
+    }
+
+    stopInterval() {
+        clearInterval(this.intervalID);
+        this.intervalID = null;
+    }
+
+    async loadDB() {
+        try {
+            await fs.promises.access(this.dataFilePath);
+            this.data = JSON.parse(await fs.promises.readFile(this.dataFilePath));
+        } catch {
+            this.data = null;
+        }
+    }
+
+    async updateDB(status) {
+        try {
+            // TODO I should just append to the file and not rewriting it.
+            // Maybe just keep the past 3 months too.
+            this.data.push({date: new Date(), alive: status});
+            await fs.promises.writeFile(this.dataFilePath, JSON.stringify(this.data));
+        } catch (error) {
+            console.log("Error while trying to write to DB.");
+        }
+    }
+
+    async createFoldersIfNeeded() {
+        try {
+            await fs.promises.access(this.dataFolderPath);
+        } catch {
+            try {
+                await fs.promises.mkdir(this.dataFolderPath, { recursive: true });
+            } catch {
+                console.log("Error while creating the data folder");
+                process.exit(1);
+            }
+        }
+    }
+}
