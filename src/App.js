55b4e2d3883b15841de6231ea3567dfecfd0c104
Mathis Engels <mathisengels@yahoo.fr>
2021-09-20 22:01:53 +0200

    Refractor w/ prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { Toaster } from 'react-hot-toast';
 import useToken from './hooks/useToken';
-import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 
 function App() {
   const { token, setToken, deleteToken } = useToken();
   return (
     <>
-    {
-    !token ?
-    <Login setToken={setToken} />
-    :
-    <Dashboard token={token} deleteToken={deleteToken} />
-    }
-    <Toaster position="bottom-right"/>
+      {
+        !token ?
+          <Login setToken={setToken} />
+          :
+          <Dashboard token={token} deleteToken={deleteToken} />
+      }
+      <Toaster position="bottom-right" />
     </>
   )
 }
