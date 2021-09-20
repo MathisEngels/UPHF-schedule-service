2240fb7eecf8eea1f81be56f73a653d2561fb4ec
Mathis Engels <mathisengels@yahoo.fr>
2021-09-20 23:09:47 +0200

    Added the statusChecker and refractor (prettier)

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require("path");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
-const UPHFScheduleScraper = require("./scraper");
+const UPHFScheduleScraper = require("./uphf/scraper");
+const UPHFStatusChecker = require("./uphf/statusChecker");
 require("dotenv").config();
 
 const downloadPath = path.join(__dirname, "download");
 const dataPath = path.join(__dirname, "data");
+const statusChecker = new UPHFStatusChecker(dataPath, 5);
 const CDSI = new UPHFScheduleScraper(downloadPath, dataPath, "CDSI", 30, 12 * 60);
 const MEEF = new UPHFScheduleScraper(downloadPath, dataPath, "MEEF", 30, 12 * 60);
 
@@ -15,37 +17,38 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 const authGuard = (req, res, next) => {
-    let token = req.headers['authorization'];
-    if (typeof token !== 'undefined') {
+    let token = req.headers["authorization"];
+    if (typeof token !== "undefined") {
         try {
             token = jwt.verify(token, process.env.JWT_SECRET);
             req.token = token;
-    
+
             return next();
-        } catch(err) {
+        } catch (err) {
             return res.status(401).end();
         }
     } else {
         res.status(401).end();
     }
-}
+};
 const roleGuard = (req, res, next) => {
+    if (req.token.role) return next();
     switch (req.params.classname.toLowerCase()) {
         case "cdsi":
-            if (req.token.role === "cdsi" || req.token.role === "admin") return next();
+            if (req.token.role === "cdsi") return next();
             break;
         case "meef":
-            if (req.token.role === "meef" || req.token.role === "admin") return next();
+            if (req.token.role === "meef") return next();
             break;
-        default: 
+        default:
             return res.status(400).end();
     }
     return res.status(401).end();
-}
+};
 const adminOnly = (req, res, next) => {
     if (req.token.role === "admin") return next();
     return res.status(401).end();
-}
+};
 
 app.post("/api/auth", async (req, res) => {
     const { password } = req.body;
@@ -61,21 +64,25 @@ app.post("/api/auth", async (req, res) => {
         if (meefMatch) role = "meef";
 
         const token = jwt.sign({ uuid: uuidv4(), role: role }, process.env.JWT_SECRET, { expiresIn: "365 days" });
-        res.status(200).json({ token: token });
+        return res.status(200).json({ token: token });
     }
-    res.status(401).end();
+    return res.status(401).end();
 });
 app.post("/api/verify", authGuard, (_, res) => {
     return res.status(200).end();
 });
-app.get("/api/get/:classname", authGuard, roleGuard, async (req, res) => {
+app.get("/api/status", authGuard, (_, res) => {
+    const data = statusChecker.toObject();
+    return res.status(200).json(data);
+});
+app.get("/api/get/:classname", authGuard, roleGuard, (req, res) => {
     let data;
     switch (req.params.classname.toLowerCase()) {
         case "cdsi":
-            data = await CDSI.toObject();
+            data = CDSI.toObject();
             break;
         case "meef":
-            data = await MEEF.toObject();
+            data = MEEF.toObject();
             break;
         default:
             data = null;
@@ -87,24 +94,25 @@ app.get("/api/get/:classname", authGuard, roleGuard, async (req, res) => {
 app.post("/api/update/:classname", authGuard, adminOnly, async (req, res) => {
     switch (req.params.classname.toLowerCase()) {
         case "cdsi":
-            await CDSI.updateAndResetLoop();
+            await CDSI.restart();
             break;
         case "meef":
-            await MEEF.updateAndResetLoop();
+            await MEEF.restart();
             break;
         default:
-            await CDSI.updateAndResetLoop();
-            await MEEF.getData();
+            await CDSI.restart();
+            await MEEF.restart();
             break;
     }
-    res.status(200).end();
+    return res.status(200).end();
 });
 app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
+    return res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 app.listen(process.env.PORT, async () => {
     console.log(`Server running on port ${process.env.PORT}`);
+    await statusChecker.start();
     await CDSI.init();
     await MEEF.init();
     // await CDSI.run();
